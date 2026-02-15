43d76ce12e17bf80aec8b6472a1551732bae21b0
Wyatt Shore <wynshore@gmail.com>
2026-02-15 13:00:35 -0800

    Removed duplicate app folder

--- bird_feeder_app/android/app/src/main/kotlin/com/example/bird_feeder_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.bird_feeder_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- bird_feeder_app/lib/globals.dart
+++
@@ -1,2 +0,0 @@
-// This stores the Pi's IP address globally so all screens can use it
-String currentPiIp = "10.0.0.47"; // Default/Fallback
